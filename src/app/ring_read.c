30d70543b1a01b18fb29456869fe061606a6aaa5
Tom St Denis <tom.stdenis@amd.com>
2017-07-13 09:34:34 -0400

    Add VCN rings to PM4 supported rings.
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/app/ring_read.c
+++ src/app/ring_read.c
@@ -52,6 +52,9 @@ void umr_read_ring(struct umr_asic *asic, char *ringpath)
 	memset(&decoder, 0, sizeof decoder);
 	if (!memcmp(ringname, "gfx", 3) ||
 	    !memcmp(ringname, "uvd", 3) ||
+	    !memcmp(ringname, "vcn_dec", 7) ||
+	    !memcmp(ringname, "vcn_enc", 7) ||
+	    !memcmp(ringname, "vcn_enc", 7) ||
 	    !memcmp(ringname, "comp", 4)) {
 		enable_decoder = 1;
 		decoder.pm = 4;
